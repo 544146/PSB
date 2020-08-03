2630c0a7e052fdc3bdadbdff25bc4631394122a5
544146 <22506439+544146@users.noreply.github.com>
2020-08-03 22:24:26 +0100

    Update Telegram.py

--- src/Telegram.py
+++ src/Telegram.py
@@ -16,3 +16,12 @@ class Sender:
             'https://api.telegram.org/bot{}/sendMessage'.format(
                 self.tg_bot_key),
             params=params)
+    
+    def edit(self, message, message_id):
+
+        params = (('text', message), ('chat_id', self.chat_id), ('message_id', message_id))
+
+        return requests.get(
+            'https://api.telegram.org/bot{}/editMessageText'.format(
+                self.tg_bot_key),
+            params=params)
